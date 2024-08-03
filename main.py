dd99a8919efb21fa859c130cd642e113ca37cfb7
mgarrod <45399726+mgarrod193@users.noreply.github.com>
2024-08-03 23:03:38 +0100

    created intial window

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 app = QApplication([])
+
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        self.setWindowTitle("Number Guesser")
+
+        button = QPushButton("Press Me!")
+
+        self.setCentralWidget(button)
+#creates and shows window
+window = MainWindow()
+window.show()
+
+#runs loop
+app.exec()
